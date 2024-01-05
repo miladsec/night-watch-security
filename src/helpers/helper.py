7c181537d2145f4403826e8d72ea37ccb626abea
Milad Dehghan <milad.dehghan@hotmail.com>
2024-01-05 12:54:39 +0330

    add utf-8 methods to helper

--- src/helpers/helper.py
+++ src/helpers/helper.py
@@ -9,6 +9,24 @@ def get_today_string():
     return current_date
 
 
+def is_utf8(encoded_text):
+    try:
+        decoded_text = encoded_text.decode('utf-8')
+        return True
+    except UnicodeDecodeError:
+        return False
+
+
+def remove_non_utf8_lines(file_path):
+    with open(file_path, 'rb') as file:
+        lines = file.readlines()
+
+    utf8_lines = [line for line in lines if is_utf8(line)]
+
+    with open(file_path, 'wb') as file:
+        file.writelines(utf8_lines)
+
+
 def read_base_yaml():
     base_path = os.path.join(os.getcwd(), 'src', 'config', 'base.yml')
     with open(base_path, 'r') as file:
